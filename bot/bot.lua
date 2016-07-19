cc75eb84decf8ade8bbff627cba59d70cf4b8d47
Zedspamirantop <amirlio1289@gmail.com>
2016-07-20 01:09:33 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {169111417},
+    sudo_users = {0000000000},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
